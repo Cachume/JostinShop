eec3279a11d3041e46a7df3f276c60032e6f1a39
cachume <albertq703@gmail.com>
2024-12-22 21:37:47 -0400

    Added Admin Page

---
+++ admin/admin.js
@@ -0,0 +1,8 @@
+
+function openMenu() {
+    document.getElementById("header_menu").style.left= "0px";
+}
+
+function closeMenu() {
+    document.getElementById("header_menu").style.left = "-300px"
+}
